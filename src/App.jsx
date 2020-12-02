d15405a6a52caa5d3708e8fab8eb9ce1d29976f1
Turkay Tunc <turkaytunc00@gmail.com>
2020-12-02 12:28:18 +0300

    add: test folder, moved test files to test folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
 import './app.scss';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>MeinShop</h1>
+    </div>
+  );
 }
 
 export default App;
