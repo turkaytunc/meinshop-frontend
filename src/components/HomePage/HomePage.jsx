b698fc2c8dabf9337f66f6f129bf78c0acceb166
Turkay Tunc <turkaytunc00@gmail.com>
2020-12-02 14:28:31 +0300

    add: product fetch mock

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import getProducts from '../../util/getProducts';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    getProducts().then((e) => setProducts(e));
+  }, []);
+
+  const fetchProducts = () => {
+    return (
+      <div>
+        {products
+          ? products.map((e) => (
+              <ul key={e.id}>
+                <li className="product-listitem">{e.category}</li>
+                <li className="product-listitem">{e.id}</li>
+                <li className="product-listitem">{e.title}</li>
+                <li className="product-listitem">{e.price}</li>
+                <li className="product-listitem">
+                  <img src={e.img} alt="" />
+                </li>
+                <li className="product-listitem">{e.description}</li>
+              </ul>
+            ))
+          : null}
+      </div>
+    );
+  };
+
+  return <>{fetchProducts()}</>;
 };
 
 export default HomePage;
