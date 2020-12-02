d8a5d95c27fffed011552cebf00c5fd49ff30641
Turkay Tunc <turkaytunc00@gmail.com>
2020-12-02 13:13:33 +0300

    add: navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,24 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <nav>
+      <nav className="nav-container">
+        <div className="nav-brand">MeinShop</div>
         <ul className="nav-ul">
           <li className="nav-li">
-            <Link className="nav-link"></Link>
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
           </li>
-          <li className="nav-li"></li>
+          <li className="nav-li">
+            <Link className="nav-link" to="/cart">
+              Cart
+            </Link>
+          </li>
+          <li className="nav-li">
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
