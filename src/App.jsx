7fdb7d7965678205f647702f4f5820e56b4f13d0
Turkay Tunc <turkaytunc00@gmail.com>
2020-12-02 13:09:32 +0300

    add: router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './app.scss';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
-        <h1>MeinShop</h1>
+        <Switch>
+          <Route exact path="/">
+            <h1>MeinShop</h1>
+          </Route>
+          <Route path="/cart" />
+          <Route path="/login" />
+        </Switch>
       </main>
       <Footer />
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <nav>
+        <ul className="nav-ul">
+          <li className="nav-li">
+            <Link className="nav-link"></Link>
+          </li>
+          <li className="nav-li"></li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
