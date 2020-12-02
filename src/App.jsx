665bc1b49770055c743957ed60724f54474358df
Turkay Tunc <turkaytunc00@gmail.com>
2020-12-02 12:48:01 +0300

    add: footer and header component, fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './app.scss';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>MeinShop</h1>
+      <Header />
+      <main>
+        <h1>MeinShop</h1>
+      </main>
+      <Footer />
     </div>
   );
 }
