04d6bea92c4be43409c04973a9fa3b564fc756e5
turkaytunc <turkaytunc00@gmail.com>
2020-12-07 20:31:26 +0300

    update: fetch products to use redux

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,33 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import getProducts from '../../util/getProducts';
-import Product from '../Product/Product';
-import productsFetchUrl from '../../util/productsFetchUrl';
-import './homepage.scss';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { listProducts } from "../../redux/actions/productActions";
+
+import Product from "../Product/Product";
+
+import "./homepage.scss";
 
 const HomePage = () => {
-  const [products, setProducts] = useState();
+  const products = [];
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     let isMounted = true;
-    async function fetchData() {
-      try {
-        const items = await getProducts(window.fetch, productsFetchUrl);
-        if (isMounted) setProducts(items);
-      } catch (error) {
-        console.log('Server Error');
-      }
-    }
-    fetchData();
+
+    dispatch(listProducts());
     return () => {
       isMounted = false;
     };
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="homepage-container" data-testid="home-page">
       {products
         ? products.map((e) => <Product key={e._id} product={e} />)
-        : 'Loading..'}
+        : "Loading.."}
     </div>
   );
 };
