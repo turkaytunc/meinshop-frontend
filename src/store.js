7302dc153b0f9b8e625f06b9429f60b14dec86a1
turkaytunc <turkaytunc00@gmail.com>
2020-12-07 19:52:44 +0300

    add: redux store

---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({});
+
+const initialState = {};
+
+const middleware = [thunk, logger];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
