a08e7447fe7d861f4a0159501975561c14265776
turkaytunc <turkaytunc00@gmail.com>
2021-02-08 15:03:10 +0300

    add: single product reducer

--- src/redux/reducers/productListReducer.js
+++ src/redux/reducers/productListReducer.js
@@ -2,7 +2,10 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
-} from "../constants/productConstants";
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+} from '../constants/productConstants';
 
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
@@ -14,6 +17,22 @@ export const productListReducer = (state = { products: [] }, action) => {
         products: action.payload,
       };
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (state = { product: { reviews: [] } }, action) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state };
+    case PRODUCT_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        product: action.payload,
+      };
+    case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
