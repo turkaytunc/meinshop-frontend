25012e10358c0a4dae6de882a1c855c7f9d940ec
turkaytunc <turkaytunc00@gmail.com>
2021-02-20 21:13:15 +0300

    update: test mocks

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const linkElement = screen.getByText(/MeinShop/i);
   expect(linkElement).toBeInTheDocument();
 });
